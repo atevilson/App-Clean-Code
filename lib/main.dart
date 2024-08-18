a61470a733b7177357b2625c46f9d8de9b3d919c
atevilson <freitas.atevilson@gmail.com>
2024-08-18 16:29:52 -0300

    add implementação da main

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+
+
+import 'package:clean_code_app/features/user/presentation/bloc/user_bloc.dart';
+import 'package:clean_code_app/features/user/presentation/pages/user_create_page.dart';
+import 'package:clean_code_app/features/user/presentation/pages/user_list_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  final http.Client httpClient = http.Client();
+
+  runApp(AppUsers(httpClient: httpClient));
+}
+
+class AppUsers extends StatelessWidget {
+  final http.Client httpClient;
+
+  const AppUsers({super.key, required this.httpClient});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Cadastro de usuários",
+      home: BlocProvider(
+        create: (_) => UserBloc(httpClient: httpClient),
+        child: const UserListPage(),
+      ),
+      routes: {
+        '/create': (_) => UserCreatePage(),
+        'list': (_) => const UserListPage()
+      },
+    );
+  }
+}
