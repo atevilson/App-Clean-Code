d96f56f7a7fc6a923c39d8d00498ad5a4a220c17
atevilson <freitas.atevilson@gmail.com>
2024-08-18 03:40:22 -0300

    class event, list e create req.

---
+++ lib/features/user/presentation/bloc/event/user_create_req.dart
@@ -0,0 +1,12 @@
+
+import 'package:clean_code_app/features/user/presentation/bloc/event/user_event.dart';
+
+class UserCreateReq extends UserEvent {
+  final String name;
+  final String email;
+
+  UserCreateReq({required this.name, required this.email});
+
+  @override
+  List<Object> get props => [name, email];
+}
---
+++ lib/features/user/presentation/bloc/event/user_list_requested.dart
@@ -0,0 +1,3 @@
+import 'package:clean_code_app/features/user/presentation/bloc/event/user_event.dart';
+
+class UserListRequested extends UserEvent {}
