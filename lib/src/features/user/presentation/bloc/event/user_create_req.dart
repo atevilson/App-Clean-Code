1bf7f9d9124fa51f61ae41c05fca561d0a843a89
atevilson <freitas.atevilson@gmail.com>
2024-08-25 18:50:26 -0300

    add mapeamento do campo phone

--- lib/src/features/user/presentation/bloc/event/user_create_req.dart
+++ lib/src/features/user/presentation/bloc/event/user_create_req.dart
@@ -3,8 +3,9 @@ import 'package:clean_code_app/src/features/user/presentation/bloc/event/user_ev
 class UserCreateReq extends UserEvent {
   final String name;
   final String email;
+  final String phone;
 
-  UserCreateReq({required this.name, required this.email});
+  UserCreateReq({required this.name, required this.email, required this.phone});
 
   @override
   List<Object> get props => [name, email];
