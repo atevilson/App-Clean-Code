ca8eb70d6f502a151c55b7de5df654ab77925b74
atevilson <freitas.atevilson@gmail.com>
2024-08-25 18:49:09 -0300

    add mapeamento de phone e metodo de formatação do dado

--- lib/src/features/user/data/models/user.dart
+++ lib/src/features/user/data/models/user.dart
@@ -3,14 +3,16 @@ class User {
   final int id;
   final String name;
   final String email;
+  final String phone;
 
-  User({required this.id, required this.name, required this.email});
+  User({required this.id, required this.name, required this.email, required this.phone});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'], 
       name: json['name'], 
       email: json['email'],
+      phone: json['phone']
     );
   }
   
@@ -18,8 +20,16 @@ class User {
     return {
       'id': id,
       'name': name,
-      'email': email
+      'email': email,
+      'phone': phone
     };
   } 
+
+  String get phoneFormatted {
+    if (phone.length == 11) {
+      return "(${phone.substring(0,2)}) ${phone.substring(2,7)}-${phone.substring(7)}";
+    }
+    return phone;
+  }
 
 }
