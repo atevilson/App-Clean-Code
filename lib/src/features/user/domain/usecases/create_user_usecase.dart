e6c3a2b6d69cb3f4de20431439be7d3855102ffc
atevilson <freitas.atevilson@gmail.com>
2024-09-03 12:56:26 -0300

    identação e ajustes de retorno

--- lib/src/features/user/domain/usecases/create_user_usecase.dart
+++ lib/src/features/user/domain/usecases/create_user_usecase.dart
@@ -11,22 +11,29 @@ class CreateUserUsecase {
   
   Future<User> execute({required String name, required String email, required String phone}) async {
     final response = await httpClient.post(
+
       Uri.parse('http://192.168.201.6:5000/api/users'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'name': name, 'email': email, 'phone': phone}),
+      body: jsonEncode(
+        {'name': name, 'email': email, 'phone': phone},
+      ),
     );
 
     if (response.statusCode == 200) {
+
       final user = jsonDecode(response.body);
       return User.fromJson(user);
+
     } else if (response.statusCode == 400) {
       
       final Map<String, dynamic> errorResponse = jsonDecode(response.body);
       final String errorMessage = errorResponse['erro'] ?? "Erro desconhecido";
-      throw Exception(errorMessage);
+      return throw Exception(errorMessage);
 
     } else {
-      throw Exception("Falha ao criar contato");
+
+      return throw Exception("Falha ao criar contato");
+    
     }
   }
 }
