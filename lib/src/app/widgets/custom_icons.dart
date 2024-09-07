3f032e85cb92e6282ed915b899a88f1e02ea8cf3
atevilson <atevilson.araujo@atak.com.br>
2024-09-07 20:57:12 -0300

    add widget de icons customizados

---
+++ lib/src/app/widgets/custom_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _keyFontFamily = 'CustomIcons';
+  static const IconData criarContato = IconData(59393, fontFamily: _keyFontFamily);
+  static const IconData contatos = IconData(59392, fontFamily: _keyFontFamily);
+
+}
