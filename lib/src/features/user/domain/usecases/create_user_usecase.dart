9f9062eb5ee45ca3c721f1a63c76d574d41c8aa3
atevilson <atevilson.araujo@atak.com.br>
2024-09-07 23:01:27 -0300

    add var para url e port, ajuste da URI

--- lib/src/features/user/domain/usecases/create_user_usecase.dart
+++ lib/src/features/user/domain/usecases/create_user_usecase.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 
 import 'package:clean_code_app/src/features/user/data/models/user.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class CreateUserUsecase {
@@ -10,9 +11,13 @@ class CreateUserUsecase {
   CreateUserUsecase(this.httpClient);
   
   Future<User> execute({required String name, required String email, required String phone}) async {
+
+    final String baseUrl = dotenv.env['API_URL'] ?? 'http://localhost';
+    final String port = dotenv.env['API_PORT'] ?? '3000';
+    
     final response = await httpClient.post(
 
-      Uri.parse('http://192.168.201.6:5000/api/users'),
+      Uri.parse('$baseUrl:$port/api/users'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode(
         {'name': name, 'email': email, 'phone': phone},
